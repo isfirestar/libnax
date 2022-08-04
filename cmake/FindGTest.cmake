b972845b1fd6807cf6b1cf843bcee082067695a3
zhuoyz <250603123@qq.com>
2022-08-04 16:07:08 +0800

    add FindGTest.cmake to cmake chains

---
+++ cmake/FindGTest.cmake
@@ -0,0 +1,6 @@
+# define gtest context
+# refer to cmake.org/cmake/help/v3.0/module/FindGTest.html
+set(GTEST_FOUND)
+set(GTEST_INCLUDE_DIRS /usr/include/gtest/)
+set(GTEST_LIBRARIES /usr/lib/)
+set(GTEST_BOTH_LIBRARIES /usr/lib/)
