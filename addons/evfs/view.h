a50f92c2984ae3d09389f942c131ec5b317d323e
neo.anderson <250603123@qq.com>
2023-04-07 19:46:12 +0800

    Create view.h

---
+++ addons/evfs/view.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "compiler.h"
+
+typedef struct evfs_view_node *evfs_view_pt;
+typedef void (*on_view_loaded_t)(evfs_view_pt view);
+
+extern nsp_status_t evfs_view_create(int count_of_cache_cluster);
+extern nsp_status_t evfs_view_load(on_view_loaded_t on_loaded, int count_of_cache_cluster);
+extern nsp_status_t evfs_view_expand();
+extern void evfs_view_cleanup();
+
+extern evfs_view_pt evfs_view_acquire_idle();
+extern nsp_status_t evfs_view_acquire_idle_more(int amount, evfs_view_pt *views);
+
+extern nsp_status_t evfs_view_move_to_idle(evfs_view_pt view);
+extern evfs_view_pt evfs_view_acquire_idle();
+
+extern void evfs_view_flush(evfs_view_pt view);
+
+/* view read */
+extern nsp_status_t evfs_view_read_userdata(const evfs_view_pt view, void *buffer, int offset, int length);
+
+/* view write */
+extern nsp_status_t evfs_view_write_head(evfs_view_pt view);
+extern nsp_status_t evfs_view_write_userdata(const evfs_view_pt view, const void *buffer, int offset, int length);
+
+/* change or get the next cluster id of view */
+extern nsp_status_t evfs_view_set_next_cluster_id(evfs_view_pt view, const evfs_view_pt next_view);
+extern int evfs_view_get_next_cluster_id(const evfs_view_pt view);
+
+/* change or get the user data seg size of view */
+extern nsp_status_t evfs_view_truncate_size(evfs_view_pt view, int size);
+extern int evfs_view_get_data_seg_size(const evfs_view_pt view);
+
+/* change or get the head cluster id of view */
+extern nsp_status_t evfs_view_set_head_cluster_id(evfs_view_pt view, const evfs_view_pt head_view);
+extern int evfs_view_get_head_cluster_id(const evfs_view_pt view);
+
+/* get cluster id of view */
+extern int evfs_view_get_viewid(const evfs_view_pt view);
+
+/* get view manager count status */
+extern void evfs_view_get_count(int *freeview, int *busyview);
+
+/* get the cache hit rate */
+extern float evfs_view_get_performance();
+
+/* tunnel cluster property */
+extern int evfs_view_get_max_pre_userseg();
+
+/* exchange bytes to cluster count */
+extern int evfs_view_transfer_size_to_cluster_count(int size);
+
